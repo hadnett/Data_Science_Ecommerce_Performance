41b21a865a2e3a84b892e4b4609f46efbb2ceda7
William Hadnett <williamhadnett@Williams-MBP.broadband>
2020-12-21 13:53:06 +0000

    Add explaination to number of visits by gender

--- section1_CA3_William_Hadnett.py
+++ section1_CA3_William_Hadnett.py
@@ -34,17 +34,24 @@ query = { 'Customer.Gender': 'Male' }
 result1 = shopcol.count_documents(query)
 print(result1)
 # Number of Male Visitors: 609
+# It is clear from the results returned that this ecommerce platform has considerably
+# more female visitors than male visitors.
 
 project = {'$project': {'month': {'$month': {'$toDate': '$InvoiceDate'}}}}
 match = {'$match':{ 'Customer.Gender': 'Female' }}
 group = {'$group': {'_id': '$month', 'count': {'$sum': 1}}}
 result = list(shopcol.aggregate([match, project, group]))
 print(result)
-#[{'_id': 1, 'count': 458}, {'_id': 12, 'count': 744}, {'_id': 11, 'count': 189}]
+#[{'_id': 11, 'count': 189}, {'_id': 12, 'count': 744}, {'_id': 1, 'count': 458}]
 
 project = {'$project': {'month': {'$month': {'$toDate': '$InvoiceDate'}}}}
 match = {'$match':{ 'Customer.Gender': 'Male' }}
 group = {'$group': {'_id': '$month', 'count': {'$sum': 1}}}
 result = list(shopcol.aggregate([match, project, group]))
 print(result)
-#[{'_id': 12, 'count': 308}, {'_id': 11, 'count': 57}, {'_id': 1, 'count': 244}]
+#[{'_id': 11, 'count': 57}, {'_id': 12, 'count': 308}, {'_id': 1, 'count': 244}]
+
+# After reviewing the visits by gender grouped by month we can see that both males
+# and females visited the site more during the month of December. This makes
+# sense as customers were most likely buying Christmas gifts during this month.
+# However, overall per month females appear to visit the site more.  
