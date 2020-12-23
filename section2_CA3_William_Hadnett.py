92f793ec3f8ccb6efa775d399829d628bb14df73
hadnett <williamsrhadnett@outlook.com>
2020-12-23 02:21:37 +0000

    Fix output to improve readability

--- section2_CA3_William_Hadnett.py
+++ section2_CA3_William_Hadnett.py
@@ -90,23 +90,19 @@ def findAssoication(mongoResponse):
     
     group = {'$group': {'_id': 0, 'total': {'$sum': 1}}}
     totalDocs = list(shopcol.aggregate([group]))
-    print(totalDocs)
     
     for i in pairs:
         # Support(x) =  # of transactions in which x appears/total transactions
-        
         query =  {'Basket.StockCode': i[0]['_id']}
         supportItem1 = shopcol.count_documents(query) / totalDocs[0]['total']
-        print('Support Item 1: ',supportItem1)
-        # Support 85123A bought:  0.153 
-        
+        print('Support for Item ',i[0]['_id'],': ',supportItem1)
+
         query =  {'Basket.StockCode': {'$all': [i[0]['_id'], i[1]['_id']]}}
         supportBoth = shopcol.count_documents(query) / totalDocs[0]['total']
         print('Support Both: ',supportBoth)
-        # Support Both bought: 0.0105
         
-        #Confidence that 21212 will be bought when 85123A is bought.
-        #Conf(85123A -> 21212) = supp(85123A and 21212)/ supp(85123A)
+        #Confidence that Item 1 will be bought when Item 2 is bought.
+        #Conf(Item 1 -> Item 2) = supp(Item 1 and Item 2)/ supp(Item 1)
         
         conf = supportBoth / supportItem1
         print('Confidence: ',conf)
@@ -115,12 +111,12 @@ def findAssoication(mongoResponse):
         # Lift 
         query =  {'Basket.StockCode': i[1]['_id']}
         supportItem2 = shopcol.count_documents(query) / totalDocs[0]['total']
-        print('Support Item 2: ',supportItem2)
+        print('Support Item ',i[1]['_id'],': ',supportItem2)
         # 0.056
         
-        #Life(85123A -> 21212) = supp(85123A and 21212)/ supp(85123A) * supp(21212)
+        #Lift(Item 1 -> Item 2) = supp(Item 1 and Item 2)/ supp(Item1) * supp(Item2)
         lift = supportBoth / (supportItem1 * supportItem2)
-        print("Lift: ",lift)
+        print('Lift ',i[0]['_id'],' -> ',i[1]['_id'],': ',lift)
         
         print("\n")
 
